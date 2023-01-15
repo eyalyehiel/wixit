a62c186647e1d29fa283505d921326261098318a
ShonChassel <gershoni1234@gmail.com>
2023-01-15 14:01:42 +0200

    first update

---
+++ src/components/app-header.vue
@@ -0,0 +1,27 @@
+<template>
+    <img src="../assets/svg/wix-logo.svg.svg" alt="">
+    
+</template>
+
+<script>
+
+export default {
+  props: {
+    
+  },
+  data() {
+    return {
+      
+    };
+  },
+  created() {
+   
+  },
+  methods: {
+    
+  },
+  components: {
+   
+    },
+};
+</script>
