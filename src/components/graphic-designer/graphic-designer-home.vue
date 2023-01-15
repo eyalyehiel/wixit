21275bfb990359cc436aa7e21c2f90ffe27ce4f8
Guy Barda <guy.barda.1234@gmail.com>
2023-01-15 17:08:41 +0200

    graphic designer

---
+++ src/components/graphic-designer/graphic-designer-home.vue
@@ -0,0 +1,15 @@
+<template>
+    <graphic-designer-header />
+</template>
+
+<script>
+import greaphicDesignerHeader from './greaphic-designer-header.vue';
+export default {
+    setup() {
+
+    },
+    components: {
+        greaphicDesignerHeader
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import siteApp from '../views/site-app.vue'
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/site',
-      name: 'site',
-      component: siteApp
-    }
-  ]
-})
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import siteApp from '../views/site-app.vue';
+import graphicDesignerHome from '../components/graphic-designer/graphic-designer-home';
 
-export default router
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/site',
+            name: 'site',
+            component: siteApp,
+        },
+        {
+            path: '/site/:id',
+            name: 'sitePreview',
+            component: graphicDesignerHome,
+        },
+    ],
+});
+
+export default router;
