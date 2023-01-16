f2c08c5f7960f65960fee2b1051d68d46b68532b
Eyal Yehiel <eyalyhi@gmail.com>
2023-01-16 19:47:06 +0200

    added display size to site display

--- src/views/site-edit.vue
+++ src/views/site-edit.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="site-edit" :class="{open : isOpen.value}">
+    <section class="site-edit" :class="{ open: isOpen.value }">
         <nav class="editor-header">
             <section class="options">
                 <button>
@@ -11,35 +11,28 @@
                 <button>
                     <img src="../assets/svg/arrow-90deg-right.svg" alt="" />
                 </button>
-                <button>
+                <button @click="toggleDisplaySize('desktop')">
                     <img src="../assets/svg/display.svg" alt="" />
                 </button>
-                <button>
+                <button @click="toggleDisplaySize('tablet')">
                     <img src="../assets/svg/tablet.svg" alt="" />
                 </button>
-                <button>
+                <button @click="toggleDisplaySize('phone')">
                     <img src="../assets/svg/phone.svg" alt="" />
                 </button>
             </section>
             <section class="publish">
-                <p>http://127.0.0.1:5173/#/site/<span contenteditable="true">HamburgerShop</span></p>
+                <p>
+                    http://127.0.0.1:5173/#/site/<span contenteditable="true"
+                        >HamburgerShop</span
+                    >
+                </p>
                 <button>Preview</button>
                 <button>Publish</button>
             </section>
-            <!-- <div class="options"> -->
-            <!-- <section class="work-together gap"> -->
-            <!-- </section> -->
-
-            <!-- <section class="history-options gap"> -->
-            <!-- </section> -->
-
-            <!-- <section class="media-select gap"> -->
-            <!-- </section> -->
-            <!-- </div> -->
         </nav>
 
         <section class="editor-sidebar">
-
             <nav class="editor-nav">
                 <button @click="toggleMenu()">
                     <img src="../assets/svg/plus-lg.svg" alt="" />
@@ -52,7 +45,7 @@
                 </button>
             </nav>
 
-            <section class="section-select" :class="{open : isOpen.value}">
+            <section class="section-select" :class="{ open: isOpen.value }">
                 <h2 class="title">Section</h2>
                 <span>Header</span>
                 <span>Hero</span>
@@ -65,7 +58,7 @@
             </section>
         </section>
 
-        <section class="main-site-display">
+        <section class="site-display" :class="displaySize">
             <pre>
               {{ siteToEdit.cmps }}
         </pre
@@ -75,25 +68,29 @@
 </template>
 
 <script setup>
-import { onMounted, reactive, ref } from "vue";
-import { useRoute } from "vue-router";
-import { siteService } from "../services/site-service.js";
+import { onMounted, reactive, computed, watch, ref } from "vue"
+import { useRoute } from "vue-router"
+import { siteService } from "../services/site-service.js"
 
-let siteToEdit = ref({});
-let isOpen = reactive({value : false})
-const route = useRoute();
+let siteToEdit = ref({})
+let isOpen = reactive({ value: false })
+let displaySize = ref("desktop")
+
+function toggleDisplaySize(val) {
+    displaySize.value = val
+}
+const route = useRoute()
 
 onMounted(async () => {
-    const { id } = route.params;
+    const { id } = route.params
     siteToEdit.value = id
         ? await siteService.getById(id)
-        : siteService.getEmptySite();
-    console.log("this.siteToEdit", siteToEdit);
-});
+        : siteService.getEmptySite()
+    console.log("this.siteToEdit", siteToEdit)
+})
 
 function toggleMenu() {
     isOpen.value = !isOpen.value
-    console.log(isOpen);
+    console.log(isOpen)
 }
-
 </script>
