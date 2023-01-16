66b76bd8fa32d195cf84bc7b1fc1b3bb829215a8
ShonChassel <gershoni1234@gmail.com>
2023-01-16 16:29:46 +0200

    first update

--- src/views/site-edit.vue
+++ src/views/site-edit.vue
@@ -1,16 +1,15 @@
 <template>
     <section class="site-edit">
-
         <nav class="editor-header">
             <section class="options">
                 <button>
                     <img src="../assets/svg/people.svg" alt="" />
                 </button>
                 <button>
-                    <img src="../assets/svg/arrow-return-left.svg" alt="" />
+                    <img src="../assets/svg/arrow-90deg-left.svg" alt="" />
                 </button>
                 <button>
-                    <img src="../assets/svg/arrow-return-right.svg" alt="" />
+                    <img src="../assets/svg/arrow-90deg-right.svg" alt="" />
                 </button>
                 <button>
                     <img src="../assets/svg/display.svg" alt="" />
@@ -23,69 +22,55 @@
                 </button>
             </section>
             <section class="publish">
-              <input type="text">
-              <button>Preview</button>
-              <button>Publish</button>
+                <input type="text" />
+                <button>Preview</button>
+                <button>Publish</button>
             </section>
             <!-- <div class="options"> -->
             <!-- <section class="work-together gap"> -->
             <!-- </section> -->
 
-            <div class="options">
+            <!-- <section class="history-options gap"> -->
+            <!-- </section> -->
 
-                <section class="work-together gap">
-                    <img src="../assets/svg/people.svg" alt="">
-                </section>
-
-                <section class="history-options gap">
-                    <img src="../assets/svg/arrow-90deg-left.svg" alt="">
-                    <img src="../assets/svg/arrow-90deg-right.svg" alt="">
-                </section>
-
-                <section class="media-select gap">
-                    <img src="../assets/svg/display.svg" alt="">
-                    <img src="../assets/svg/tablet.svg" alt="">
-                    <img src="../assets/svg/phone.svg" alt="">
-                </section>
-
-            </div>
+            <!-- <section class="media-select gap"> -->
+            <!-- </section> -->
+            <!-- </div> -->
         </nav>
 
         <section class="editor-sidebar">
 
             <nav class="editor-nav">
-                <img src="../assets/svg/plus-lg.svg" alt="">
-                <img src="../assets/svg/palette.svg" alt="">
-                <img src="../assets/svg/file-richtext.svg" alt="">
+                <img src="../assets/svg/plus-lg.svg" alt="" />
+                <img src="../assets/svg/palette.svg" alt="" />
+                <img src="../assets/svg/file-richtext.svg" alt="" />
             </nav>
-            
+
         </section>
 
+
         <section class="main-site-display">
-        <pre>
+            <pre>
               {{ siteToEdit.cmps }}
-        </pre>
-       </section>
-
+        </pre
+            >
+        </section>
     </section>
-
-    
-
 </template>
 
 <script setup>
-import { onMounted, reactive, ref } from "vue"
-import { useRoute } from "vue-router"
-import { siteService } from "../services/site-service.js"
+import { onMounted, reactive, ref } from "vue";
+import { useRoute } from "vue-router";
+import { siteService } from "../services/site-service.js";
 
-let siteToEdit = ref({})
-const route = useRoute()
+let siteToEdit = ref({});
+const route = useRoute();
 
 onMounted(async () => {
-    const { id } = route.params
+    const { id } = route.params;
     siteToEdit.value = id
         ? await siteService.getById(id)
-        : siteService.getEmptySite()
-    console.log("this.siteToEdit", siteToEdit)
-})
+        : siteService.getEmptySite();
+    console.log("this.siteToEdit", siteToEdit);
+});
 </script>
