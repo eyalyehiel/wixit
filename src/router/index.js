a04a8a494585d6c84e30f62516381cb5bcb44a08
Guy Barda <guy.barda.1234@gmail.com>
2023-01-16 10:41:36 +0200

    first

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,7 @@
-<<<<<<< HEAD
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import siteApp from '../views/site-app.vue'
-import templates from '../views/templates.vue'
-
-const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/site',
-      name: 'site',
-      component: siteApp
-    },
-    {
-      path: '/templates',
-      name: 'templates',
-      component: templates
-    }
-  ]
-})
-=======
 import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/home-view.vue';
 import siteApp from '../views/site-app.vue';
 import graphicDesignerHome from '../components/graphic-designer/graphic-designer-home.vue';
->>>>>>> 6bfb81b565b5589b48c7febd706c86af80a3e8ec
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
