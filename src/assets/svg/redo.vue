0d1eeac4738f6340d4fcbc601cf548f931e8c975
Eyal Yehiel <eyalyhi@gmail.com>
2023-01-20 17:02:40 +0200

    fixed styling in site edit header

---
+++ src/assets/svg/redo.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 29 29"
+        class="symbol symbol-undo_NewWorkspace btn-symbol"
+    >
+        <path
+            fill="currentColor"
+            d="M14 11H6.83l3-3.33L9.11 7 5 11.51 9.11 16l.74-.68L6.8 12H14a5 5 0 010 10h-2v1h2a6 6 0 100-12z"
+        />
+    </svg>
+</template>
---
+++ src/assets/svg/undo.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 29 29"
+        class="symbol symbol-redo_NewWorkspace btn-symbol"
+    >
+        <path
+            fill="currentColor"
+            d="M15 11h7.17l-3-3.33.72-.67L24 11.51 19.89 16l-.74-.68L22.2 12H15a5 5 0 000 10h2v1h-2a6 6 0 110-12z"
+        />
+    </svg>
+</template>
