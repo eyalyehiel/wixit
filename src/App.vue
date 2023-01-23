b12f50c47030c6e35b85c43c98f1aa475c6da16b
ShonChassel <gershoni1234@gmail.com>
2023-01-23 16:43:01 +0200

    first update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script setup>
+
 import { RouterView } from 'vue-router'
 import appHeader from './components/app-header.vue';
 
--- src/components/login.vue
+++ src/components/login.vue
@@ -30,14 +30,14 @@
                     <span>Continue with Google</span>
                 </button>
 
-                <button @click="loginWithFacebook()">
+                <button @click="logInWithFacebook()">
                     <div class="buttonIcon">
                         <img src="../assets/svg/facebook-logo.svg" alt="" />
                     </div>
                     <span>Continue with Facebook</span>
                 </button>
 
-                <button>
+                <button @click="logInOut()">
                     <div class="buttonIcon last">
                         <img src="../assets/svg/apple-logo.svg" alt="" />
                     </div>
@@ -49,25 +49,33 @@
 </template>
 
 <script>
-import { initFbsdk } from "../services/facebook_oAuth.js";
+import facebookLogin from "facebook-login-vuejs";
+
+// import { initFbsdk } from "../services/facebook_oAuth.js";
 import { inject } from "vue";
 import axios from "axios";
 
 export default {
+    name: "facebookLogin",
     props: {},
+    components: {
+        facebookLogin,
+    },
     data() {
         return {};
     },
+
     setup() {
         const Vue3GoogleOauth = inject("Vue3GoogleOauth");
         return {
             Vue3GoogleOauth,
         };
     },
-    mounted() {
-        initFbsdk();
+    mounted() {},
+    created() {
+        
+        
     },
-    created() {},
     methods: {
         async signIn() {
             try {
@@ -82,23 +90,46 @@ export default {
         openSignUp() {
             this.$router.push("/signup");
         },
-       
-        async loginWithFacebook() {
-            console.log("hey");
-            try {
-                const { authResponse } = await new Promise(FB.login);
-                if (!authResponse) return router.push({ path: "/" });
-
-                const accessToken = authResponse.accessToken;
-                const response = await axios.facebookAuth(accessToken);
-
-                console.log('response',response )
-                // localStorage.setItem("token", response.token);
-                // localStorage.setItem("refreshToken", response.refreshToken);
-                // localStorage.setItem("user", response.User._id);
-            } catch (error) {
-                console.log(error);
+        async logInWithFacebook() {
+            await this.loadFacebookSDK(document, "script", "facebook-jssdk");
+            await this.initFacebook();
+            window.FB.login(function (response) {
+                if (response.authResponse) {
+                    alert("You are logged in &amp; cookie set!");
+                } else {
+                    alert("User cancelled login or did not fully authorize.");
+                }
+            });
+            return false;
+        },
+        logInOut() {
+            FB.getLoginStatus(function (response) {
+                FB.logout(function (response) {
+                    console.log("Logged Out!");
+                    // window.location = "/";
+                });
+            });
+        },
+        async initFacebook() {
+            console.log('init')
+            window.fbAsyncInit = function () {
+                window.FB.init({
+                    appId: "1323495125159374", //You will need to change this 1323495125159374 8220179XXXXXXXXX
+                    cookie: true, // This is important, it's not enabled by default
+                    version: "v13.0",
+                });
+            };
+        },
+        async loadFacebookSDK(d, s, id) {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
             }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://connect.facebook.net/en_US/all.js";
+            fjs.parentNode.insertBefore(js, fjs);
         },
     },
     components: {},
