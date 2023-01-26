0674c774d047bf60fac56f064c86c9257dfedebe
Eyal Yehiel <eyalyhi@gmail.com>
2023-01-26 10:55:01 +0200

    added select element option to more cmps

--- src/components/site-templates/site-footer.vue
+++ src/components/site-templates/site-footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <footer class="site-footer" :style="cmp.info.style">
-        <h1 :style="cmp.info.title.style">{{ cmp.info.title.text }}</h1>
-        <p :style="cmp.info.subtitle.style">
+    <footer class="graphic-designer-footer" @click.stop="onEditElement($event, 'site-footer')" :style="cmp.info.style">
+        <h1 :style="cmp.info.title.style" @click.stop="onEditElement($event, 'title')">{{ cmp.info.title.text }}</h1>
+        <p :style="cmp.info.subtitle.style" @click.stop="onEditElement($event, 'subtitle')">
             {{
                 cmp.info.subtitle.text ||
                 "CoffeRights 2035 by. created with Wixit"
@@ -14,4 +14,8 @@
 import { defineProps } from "vue"
 
 const { cmp } = defineProps({ cmp: Object })
+const emit = defineEmits(["onChangeText", "editElement"])
+function onEditElement(ev, key) {
+    emit("editElement", key)
+}
 </script>
