4faf4fc508bd46bffd179aaf2d862e167f95eff9
ShonChassel <gershoni1234@gmail.com>
2023-01-31 16:54:47 +0200

    first update

--- src/components/site-templates/site-section.vue
+++ src/components/site-templates/site-section.vue
@@ -1,24 +1,42 @@
 <template>
-    <section class="site-section" @click.stop="onEditElement($event, 'site-section')" :style="cmp.style"
-        style="background-size: cover;">
+    <section
+        class="site-section"
+        @click.stop="onEditElement($event, 'site-section')"
+        :style="cmp.style"
+        style="background-size: cover"
+    >
+        <div class="txt-section">
+            <h2
+                :style="cmp.info.title.style"
+                @click.stop="onEditElement($event, 'title')"
+            >
+                {{ cmp.info.title.text }}
+            </h2>
+            <p
+                :style="cmp.info.paragraph1.style"
+                @click.stop="onEditElement($event, 'paragraph1')"
+            >
+                {{ cmp.info.paragraph1.text }}
+            </p>
+            <p
+                :style="cmp.info.paragraph2.style"
+                @click.stop="onEditElement($event, 'paragraph2')"
+            >
+                {{ cmp.info.paragraph2.text }}
+            </p>
+        </div>
 
-        <h2 :style="cmp.info.title.style" @click.stop="onEditElement($event, 'title')">{{ cmp.info.title.text }}</h2>
-        <p :style="cmp.info.paragraph1.style" @click.stop="onEditElement($event, 'paragraph1')">
-            {{ cmp.info.paragraph1.text }}</p>
-        <p :style="cmp.info.paragraph2.style" @click.stop="onEditElement($event, 'paragraph2')">
-            {{ cmp.info.paragraph2.text }}</p>
-            
-        <img v-for="img in cmp.info.images" :src="img" alt="">
+        <img v-for="img in cmp.info.images" :src="img" alt="" />
     </section>
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from "vue"
+import { defineProps, defineEmits } from "vue";
 
-const { cmp } = defineProps({ cmp: Object })
-const emit = defineEmits(["onChangeText", "editElement"])
+const { cmp } = defineProps({ cmp: Object });
+const emit = defineEmits(["onChangeText", "editElement"]);
 
 function onEditElement(ev, key) {
-    emit("editElement", key)
+    emit("editElement", key);
 }
 </script>
