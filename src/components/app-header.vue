236e026521a97709cc1170d37cdb138c0500977c
ShonChassel <gershoni1234@gmail.com>
2023-02-04 16:23:39 +0200

    first commit

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,35 +1,41 @@
 <template>
     <header class="main-header">
-        <!-- <img
-            @click="navigate('/')"
-            src="../assets/svg/wix-logo.svg.svg"
-            alt=""
-        /> -->
-        <p class="logo"  @click="navigate('/')">w<span>ixit</span></p>
+
+        <div class="logo">
+            <img
+                @click="navigate('/')"
+                src="../assets/svg/logo3.png"
+                alt=""
+            />
+            <p>wixit</p>
+            <!-- <img src="../assets/svg/unnamed (1).png" alt="" /> -->
+        </div>
+        <!-- <p class="logo"  @click="navigate('/')">w<span>ixit</span></p> -->
 
         <div class="header-btns">
-            <button v-if="!isLoginPath" @click="navigate('/login')">Log In</button>
+            <button v-if="!isLoginPath" @click="navigate('/login')">
+                Log In
+            </button>
             <button @click="navigate('/site')">Get Started</button>
         </div>
-
-
     </header>
 </template>
 
 <script setup>
-import { watch, ref } from "vue"
-import { useRouter, useRoute } from "vue-router"
+import { watch, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
 
-const router = useRouter()
-const route = useRoute()
-const isLoginPath = ref(false)
+const router = useRouter();
+const route = useRoute();
+const isLoginPath = ref(false);
 
 function navigate(to) {
-    router.push(to)
+    router.push(to);
 }
 
 watch(route, () => {
-    if (route.path === "/login" || route.path === "/signup") isLoginPath.value = true
-    else isLoginPath.value = false
-})
+    if (route.path === "/login" || route.path === "/signup")
+        isLoginPath.value = true;
+    else isLoginPath.value = false;
+});
 </script>
