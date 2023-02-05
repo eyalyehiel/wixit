123ba57d969cbf439c1e323e0d85f15a0010928f
Guy Barda <guy.barda.1234@gmail.com>
2023-02-05 11:26:55 +0200

    content editable for all

--- src/components/site-templates/site-footer.vue
+++ src/components/site-templates/site-footer.vue
@@ -1,7 +1,9 @@
 <template>
     <footer class="site-footer" @click.stop="onEditElement($event, 'site-footer')" :style="cmp.info.style">
-        <h1 :style="cmp.info.title.style" @click.stop="onEditElement($event, 'title')">{{ cmp.info.title.text }}</h1>
-        <p :style="cmp.info.subtitle.style" @click.stop="onEditElement($event, 'subtitle')">
+        <h1 :style="cmp.info.title.style" @click.stop="onEditElement($event, 'title')" :contenteditable="isEdit">{{
+            cmp.info.title.text
+        }}</h1>
+        <p :style="cmp.info.subtitle.style" @click.stop="onEditElement($event, 'subtitle')" :contenteditable="isEdit">
             {{
     cmp.info.subtitle.text ||
         "CoffeRights 2035 by. created with Wixit"
@@ -13,8 +15,10 @@
 <script setup>
 import { defineProps } from "vue"
 
-const { cmp } = defineProps({ cmp: Object })
+const { cmp, isEdit } = defineProps({ cmp: Object, isEdit: Boolean })
+
 const emit = defineEmits(["onChangeText", "editElement"])
+
 function onEditElement(ev, key) {
     emit("editElement", key)
 }
--- src/components/site-templates/site-section.vue
+++ src/components/site-templates/site-section.vue
@@ -1,27 +1,16 @@
 <template>
-    <section
-        class="site-section"
-        @click.stop="onEditElement($event, 'site-section')"
-        :style="cmp.style"
-        style="background-size: cover"
-    >
+    <section class="site-section" @click.stop="onEditElement($event, 'site-section')" :style="cmp.style"
+        style="background-size: cover">
         <div class="txt-section">
-            <h2
-                :style="cmp.info.title.style"
-                @click.stop="onEditElement($event, 'title')"
-            >
+            <h2 :style="cmp.info.title.style" @click.stop="onEditElement($event, 'title')" :contenteditable="isEdit">
                 {{ cmp.info.title.text }}
             </h2>
-            <p
-                :style="cmp.info.paragraph1.style"
-                @click.stop="onEditElement($event, 'paragraph1')"
-            >
+            <p :style="cmp.info.paragraph1.style" @click.stop="onEditElement($event, 'paragraph1')"
+                :contenteditable="isEdit">
                 {{ cmp.info.paragraph1.text }}
             </p>
-            <p
-                :style="cmp.info.paragraph2.style"
-                @click.stop="onEditElement($event, 'paragraph2')"
-            >
+            <p :style="cmp.info.paragraph2.style" @click.stop="onEditElement($event, 'paragraph2')"
+                :contenteditable="isEdit">
                 {{ cmp.info.paragraph2.text }}
             </p>
         </div>
@@ -33,7 +22,8 @@
 <script setup>
 import { defineProps, defineEmits } from "vue";
 
-const { cmp } = defineProps({ cmp: Object });
+const { cmp, isEdit } = defineProps({ cmp: Object, isEdit: Boolean });
+
 const emit = defineEmits(["onChangeText", "editElement"]);
 
 function onEditElement(ev, key) {
